ec09e6da7822a47c43a8119373c456990a813394
DistantBlue <exotic.xD@gmail.com>
2024-02-04 02:11:22 +0300

    Initial commit

---
+++ lib/main_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:triary_app/login/login_screen.dart';
+import 'package:triary_app/tabs/tabs_screen.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          FilledButton(
+            child: const Text("Offline usage"),
+            onPressed: () {
+              _navigateToTabsScreen(context);
+            },
+          ),
+          const Divider(
+            color: Colors.red,
+          ),
+          FilledButton(
+            child: const Text("Online usage"),
+            onPressed: () {
+              _navigateToLoginScreen(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  void _navigateToTabsScreen(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const TabsScreen()));
+  }
+
+  void _navigateToLoginScreen(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const LoginScreen()));
+  }
+}
---
+++ lib/tabs/tabs_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatelessWidget {
+  const TabsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.fitness_center),
+                  text: "Power training",
+                ),
+                Tab(
+                  icon: Icon(Icons.directions_run),
+                  text: "Cardio training",
+                )
+              ],
+            ),
+            title: const Text("TriaryApp"),
+          ),
+          body: const TabBarView(
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
