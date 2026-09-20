7482ee570f4b4eb6156d10736e17b4ace2fd04d4
DistantBlue <exotic.xD@gmail.com>
2026-09-20 21:28:37 +0300

    Добавляет пакеты в зависимости и выполняет перегенерацию

--- packages/entity/lib/src/training/power_training.g.dart
+++ packages/entity/lib/src/training/power_training.g.dart
@@ -8,7 +8,7 @@ part of 'power_training.dart';
 
 PowerTraining _$PowerTrainingFromJson(Map<String, dynamic> json) =>
     PowerTraining(
-      id: json['id'] as String,
+      id: json['id'] as String? ?? '',
       name: json['name'] as String,
       description: json['description'] as String,
     );
